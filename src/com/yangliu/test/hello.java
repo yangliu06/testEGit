d6be7173b1559d1e06464b20fe509f970b4abc00
yangliu06 <yangliu16hi@126.com>
2017-05-21 12:24:31 +0800

    update hello

--- src/com/yangliu/test/hello.java
+++ src/com/yangliu/test/hello.java
@@ -3,6 +3,6 @@ package com.yangliu.test;
 public class hello {
   public static void main(String[] args) {
 	System.out.println("hello word");
-	System.out.println("服务器的更新")
+	System.out.println("服务器的更新");
 }
 }
